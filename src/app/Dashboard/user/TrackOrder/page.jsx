a53b77e276b42f76c23af37db0f3c9dc16388c4c
rihadabib-commits <rihadabib@gmail.com>
2026-07-09 01:56:07 +0600

    issue

--- src/app/Dashboard/user/TrackOrder/page.jsx
+++ src/app/Dashboard/user/TrackOrder/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const TrackOrder = () => {
     return (
         <div>
-            <OrderTrackingPage></OrderTrackingPage>
+            track order page
         </div>
     );
 };
