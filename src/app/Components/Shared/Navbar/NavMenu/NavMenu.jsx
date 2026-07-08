c2ae8bc648f59ef22ba30843fe91d04c450cb5d1
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-08 14:07:09 +0600

    more work done

--- src/app/Components/Shared/Navbar/NavMenu/NavMenu.jsx
+++ src/app/Components/Shared/Navbar/NavMenu/NavMenu.jsx
@@ -14,6 +14,10 @@ const NavMenu = () => {
     { name: "Mens Shoes", path: "/Pages/MenShoes" },
     { name: "Home & Living", path: "/Pages/HomeLiving" },
     { name: "Gadgets", path: "/Pages/Gadgets" },
+    { name: "Shopping Cards", path: "/Pages/ShopingCards" },
+    { name: "Confirm", path: "/Pages/OrderConfirm" },
+    { name: "Success", path: "/Pages/OrderSuccess" },
+    { name: "Tracking", path: "/Pages/OrderTracking" },
   ];
 
   return (
