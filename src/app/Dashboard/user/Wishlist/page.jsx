6c65250498474b6d826db8919b96c6a022dcb021
rihadabib-commits <rihadabib@gmail.com>
2026-07-09 02:18:00 +0600

    fix

--- src/app/Dashboard/user/Wishlist/page.jsx
+++ src/app/Dashboard/user/Wishlist/page.jsx
@@ -43,7 +43,7 @@ const Wishlist = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50/50 py-10">
+    <div className="min-h-screen bg-gray-50/50 ">
       <div className="max-w-4xl mx-auto px-4">
         {wishlistItems.length > 0 ? (
           <>
