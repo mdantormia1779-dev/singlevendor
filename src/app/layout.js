3234779daf4fff08d4511cdfee0b4ce66b8397f2
torikuljony <torikuljony36@gmail.com>
2026-07-10 05:17:29 +0600

    navber update

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,11 +25,11 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-     <body className="min-h-full flex flex-col">
-  <Navbar />
-  <main>{children}</main>
-  <Footer />
-</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
