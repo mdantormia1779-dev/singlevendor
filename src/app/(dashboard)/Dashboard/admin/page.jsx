90389cd680b015b4fb50def04013060ed9a3eb1b
torikuljony <torikuljony36@gmail.com>
2026-09-03 02:09:10 +0600

    update

--- src/app/(dashboard)/Dashboard/admin/page.jsx
+++ src/app/(dashboard)/Dashboard/admin/page.jsx
@@ -1,9 +1,11 @@
+import Chart from "./Components/Chart/Chart";
 import StateCard from "./Components/StateCard/StateCard";
 
 export default function AdminDashboard() {
   return (
     <div>
       <StateCard></StateCard>
+      <Chart></Chart>
     </div>
   );
 }
